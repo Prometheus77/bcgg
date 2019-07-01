31df7d744f3cc56e50b042b5e60af07a21869202
Aaron Cooley <aaronsama@gmail.com>
2019-07-01 12:31:34 -0600

    make bc_waterfall work

--- R/bc_waterfall.R
+++ R/bc_waterfall.R
@@ -7,20 +7,82 @@
 #' @param values The column name that you want to include in the plot
 #' @param incremental Set to TRUE if the series contains incremental values to plot. Set to FALSE if the series contains actual values and incremental values must be calculated. Default is TRUE.
 #' @param anchors (optional) A column name, logical vector, or numeric vector indicating which segments should touch the x-axis.
-#' @param add.final Whether to add a final (anchored) bar to the end of the waterfall. Default is FALSE.
+#' @param anchor.final Whether to anchor the last bar in the waterfall. Default is TRUE.
+#' @param width Bar width in units, where 1 means the edges of the bars touch. Default = 0.8.
+#' @param fill.values Fill colors to use on waterfall, in order of 'pos', 'neg', 'anchor'. Default is `c('green', 'red', 'navy')`.
+#' @param text Show numerical values as text for each column? Default is `TRUE`.
+#' @param text.format Format to use to display text values as a text string. Uses `sprintf()`. Default is '%.1f', which rounds the number to one decimal place.
 #'
 #' @export
-bc_waterfall <- function(.data, labels, values, incremental = TRUE, anchors = NULL, final = FALSE) {
-  labels <- enquo(labels)
-  values <- enquo(values)
-  if (incremental == FALSE) {
+bc_waterfall <- function(.data, labels, values, incremental = TRUE, anchors = NULL, anchor.final = TRUE, width = 0.8,
+                         fill.values = c('green', 'red', 'navy'), text = TRUE, text.format = '%.1f') {
+  labels_q <- enquo(labels)
+  values_q <- enquo(values)
+
+  # ggplot2 needs a numeric x-axis
+  .data = dplyr::mutate(.data, .count = 1, .label.order = cumsum(.count))
+
+  # ggplot2 needs to display labels in the right order
+  .data = dplyr::mutate(.data, .labels = factor(!!labels_q))
+
+  if (hasArg("anchors")) {
+    anchors_q <- enquo(anchors)
+    .data = dplyr::mutate(.data, .anchors = !!anchors_q)
+  } else {
+    lastrow <- nrow(.data)
+    .data = dplyr::mutate(.data, .anchors = (.label.order %in% c(1, lastrow)))
   }
 
+  if (incremental == FALSE) {
+    .data = make_incremental(.data, !!values_q)
+    .data = dplyr::rename(.data, .abs.values = !!values_q)
+  } else {
+    .data = make_absolute(.data, !!values_q)
+    .data = dplyr::rename(.data, .incr.values = !!values_q)
+  }
+
+  .data = dplyr::mutate(.data,
+                        .lag.abs = dplyr::lag(.abs.values, 1),
+                        .lag.anchor = ifelse(.anchors, 0, .lag.abs),
+                        .ymin = pmin(ifelse(is.na(.lag.anchor), 0, .lag.anchor), .abs.values),
+                        .ymax = pmax(ifelse(is.na(.lag.anchor), 0, .lag.anchor), .abs.values),
+                        .xmin = .label.order - width / 2,
+                        .xmax = .label.order + width / 2,
+                        .direction = dplyr::case_when(.anchors == TRUE ~ 'anchor',
+                                                      .incr.values < 0 ~ 'neg',
+                                                      TRUE ~ 'pos') %>%
+                          factor(levels = c('pos', 'neg', 'anchor')),
+                        .label = sprintf(text.format, .abs.values),
+                        .label_y = ifelse(.anchors, .abs.values * 1.05, (.ymax + .ymin) / 2))
+
+  .plot = ggplot(.data, aes(x = .label.order)) +
+    geom_step(aes(y = .abs.values), linetype = "dashed") +
+    geom_rect(aes(xmin = .xmin, xmax = .xmax, ymin = .ymin, ymax = .ymax, fill = .direction), color = "black") +
+    scale_fill_manual(values = fill.values) +
+    scale_x_continuous(breaks = .data$.label.order, labels = .data$.labels, name = quo_name(labels_q)) +
+    scale_y_continuous(name = quo_name(values_q)) +
+    theme(legend.position = 'none')
+
+  if (text == TRUE) {
+    .plot = .plot + geom_text(aes(label = .label, y = .label_y))
+  }
+
+  .plot
 }
 
 make_incremental <- function(.data, values) {
   values_q = enquo(values)
-  .data %>%
-    dplyr::mutate(lag.values = lag(!!values_q),
-                  values = ifelse(is.na(lag.values), !!values_q, !!values_q - lag.values))
+  dplyr::mutate(.data,
+                lag.values = dplyr::lag(!!values_q, 1),
+                .incr.values = ifelse(is.na(lag.values), !!values_q, !!values_q - lag.values))
+}
+
+make_absolute <- function(.data, values) {
+  values_q = enquo(values)
+  dplyr::mutate(.data,
+                .abs.values = cumsum(!!values_q))
+}
+
+return_label <- function(.data, .label.order) {
+  .data$.labels[.label.order]
 }
--- tests/testthat/test-bc_waterfall.R
+++ tests/testthat/test-bc_waterfall.R
@@ -6,8 +6,17 @@ test_df <- data.frame(Label = c('2018', 'Mix', 'App lift', 'Click rate', '2019')
 test_that("make_incremental works", {
   expect_equal({
     make_incremental(test_df, Abs.Value) %>%
-      dplyr::pull(values)
+      dplyr::pull(.incr.values)
     },
-    c(500, -25, 35, 20, 0)
+    test_df$Incr.Value
     )
+})
+
+test_that("make_absolute works", {
+  expect_equal({
+    make_absolute(test_df, Incr.Value) %>%
+      dplyr::pull(.abs.values)
+  },
+  test_df$Abs.Value
+  )
 })
